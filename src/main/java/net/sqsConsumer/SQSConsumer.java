b187813bf41e6a2b8aa1118d106dcc4f0cb8ffb5
Puneet Goyal <pungoyal@gmail.com>
2013-01-14 20:52:32 +0530

    command line interface to the consumer

--- src/main/java/net/sqsConsumer/SQSConsumer.java
+++ src/main/java/net/sqsConsumer/SQSConsumer.java
@@ -2,7 +2,24 @@ package net.sqsConsumer;
 
 public class SQSConsumer {
     public static void main(String[] args) {
-        QueueConsumer queueConsumer = new QueueConsumer("https://sqs.us-east-1.amazonaws.com/302497724868/puneet-test");
-        queueConsumer.receive();
+        if (args.length == 0) {
+            QueueConsumer consumer = new QueueConsumer("https://sqs.us-east-1.amazonaws.com/302497724868/puneet-test");
+            consumer.receive();
+        } else if (args.length == 1) {
+            QueueConsumer consumer = new QueueConsumer(args[0]);
+            consumer.receive();
+        } else {
+            printUsage();
+        }
     }
+
+    private static void printUsage() {
+        System.out.println("********************************************************************************");
+        System.out.println("USAGE:");
+        System.out.println("\t java -jar <jar file> <queue name>");
+        System.out.println("eg:");
+        System.out.println("\t java -jar rest-over-sqs.jar https://sqs.us-east-1.amazonaws.com/302497724868/puneet-test");
+        System.out.println("********************************************************************************");
+    }
+
 }
--- src/test/java/net/sqsConsumer/QueueConsumerTest.java
+++ src/test/java/net/sqsConsumer/QueueConsumerTest.java
@@ -1,9 +1,11 @@
 package net.sqsConsumer;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class QueueConsumerTest {
     @Test
+    @Ignore
     public void testGetMessage() {
         QueueConsumer consumer = new QueueConsumer("https://sqs.us-east-1.amazonaws.com/302497724868/puneet-test");
         consumer.receive();
